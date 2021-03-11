406d76c9d20a83835d209735582c0e2dfc2eb8ca
dddlli <58366052+dddlli@users.noreply.github.com>
2021-03-11 15:48:41 +0800

    CenterForm

---
+++ src/controls/CenterForm.py
@@ -0,0 +1,30 @@
+#QDesktopWidget
+import sys
+from PyQt5.QtWidgets import QMainWindow,QApplication,QDesktopWidget
+from PyQt5.QtGui import QIcon
+
+class CenterForm(QMainWindow):
+    def __init__(self,parent=None):
+        super(CenterForm,self).__init__(parent)
+
+        #设置主窗口的标题
+        self.setWindowTitle('第一个主窗口应用')
+        #设置窗口的尺寸
+        self.resize(400,300)
+        self.status = self.statusBar()
+        self.status.showMessage('只存在5秒的消息',5000)
+    def center(self):
+        #获取屏幕坐标系
+        screen = QDesktopWidget().screenGeometry()
+        #获取窗口坐标系
+        size = self.geometry()
+        newLeft = (screen.width() - size.width()) / 2
+        newTop = (screen.height() - size.height()) / 2
+        self.move(newLeft, newTop)
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon('./images/c1.ico'))
+    main = CenterForm()
+    main.show()
+
+    sys.exit(app.exec_())
