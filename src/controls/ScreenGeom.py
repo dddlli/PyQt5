0cb3a04a65a332ff64a8cc78f277e77e33ea37d0
dddlli <58366052+dddlli@users.noreply.github.com>
2021-03-11 16:28:02 +0800

    ScreenGeom

---
+++ src/controls/ScreenGeom.py
@@ -0,0 +1,30 @@
+import sys
+from PyQt5.QtWidgets import QHBoxLayout,QMainWindow,QPushButton,QApplication,QWidget
+from PyQt5.QtGui import QIcon
+
+def onClick_button():
+    print("1")
+    print("widget.x() = %d" % widget.x())
+    print("widget.y() = %d" % widget.y())
+    print("widget.width() = %d" % widget.width())
+    print("widget.height() = %d" % widget.height())
+
+    print("2")
+    print("widget.geometry().x() = %d" % widget.geometry().x())
+    print("widget.geometry().y() = %d" % widget.geometry().y())
+    print("widget.geometry().width() = %d" % widget.geometry().width())
+    print("widget.geometry().height() = %d" % widget.geometry().height())
+app = QApplication(sys.argv)
+widget = QWidget()
+btn = QPushButton(widget)
+btn.setText("button")
+btn.clicked.connect(onClick_button)
+
+btn.move(24,52)
+
+widget.resize(300,240)
+widget.move(250,200)
+widget.setWindowTitle('屏幕坐标系')
+widget.show()
+app.setWindowIcon(QIcon('./images/c3.ico'))
+sys.exit(app.exec_())
